ab783896ee13919edfb875766624263fac026099
hugofsd <hugo619@outlook.com>
2025-11-01 16:11:40 -0300

    Correções de estrutura e codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
    imports: [CommonModule,
              ContainerComponent,
              CabecalhoComponent,
-             SeparadorComponent
+             SeparadorComponent,
+             ContatoComponent
             ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
